2b5d24ecbd57fc3174be2e09584d6327575c7415
dwsk <55840147+dwskme@users.noreply.github.com>
2022-06-11 23:48:46 +0545

    nvim & polybar changes

--- .config/nvim/lua/custom/plugins/override.lua
+++ .config/nvim/lua/custom/plugins/override.lua
@@ -2,27 +2,6 @@
 
 local M = {}
 
-M.treesitter = {
-   ensure_installed = {
-      "vim",
-      "html",
-      "css",
-      "javascript",
-      "json",
-      "toml",
-      "markdown",
-      "c",
-      "bash",
-      "lua",
-      "norg",
-   },
-}
-
-M.nvimtree = {
-   git = {
-      enable = true,
-   },
-}
 
 M.telescope = {
    extensions = {
@@ -44,7 +23,82 @@ M.cmp = {
    },
 }
 
+
+
+M.treesitter = {
+   ensure_installed = {
+      "vim",
+      "html",
+      "css",
+      "javascript",
+      "json",
+      "toml",
+      "markdown",
+      "c",
+      "bash",
+      "lua",
+      "norg",
+      'jsdoc',
+      'php',
+      'python',
+      'scss',
+      'tsx',
+      'typescript',
+   },
+   highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+    refactor = {
+      highlight_definitions = { enable = true },
+      highlight_current_scope = { enable = false },
+    },
+}
+
+M.nvimtree = {
+   git = {
+      enable = true,
+   },
+
+   renderer = {
+      highlight_git = true,
+      icons = {
+         show = {
+            git = true,
+         },
+      },
+   },
+   diagnostics = {
+      enable = true,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      }
+    },
+}
+
 M.blankline = {
+   indentLine_enabled = 1,
+   char = "▏",
+   show_end_of_line = true,
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
+	show_trailing_blankline_indent = false,
+	use_treesitter = true,
+   char_list = {'│'}, -- {'|', '¦', '┆', '┊'},
    filetype_exclude = {
       "help",
       "terminal",
@@ -55,9 +109,90 @@ M.blankline = {
       "TelescopeResults",
       "nvchad_cheatsheet",
       "lsp-installer",
-      "norg",
       "",
+		'startify',
+		'dashboard',
+		'neogitstatus',
+		'NvimTree',
    },
+   show_first_indent_level = false,
+   buftype_exclude = {
+		'terminal',
+		'nofile',
+	},
+	char_highlight_list = {
+		"IndentBlanklineIndent1",
+		"IndentBlanklineIndent2",
+		"IndentBlanklineIndent3",
+		"IndentBlanklineIndent4",
+		"IndentBlanklineIndent5",
+		"IndentBlanklineIndent6",
+		"IndentBlanklineIndent7",
+		"IndentBlanklineIndent8",
+    },
+}
+
+M.gitsigns ={
+   signs = {
+		add = {
+			hl = 'GitSignsAdd',
+			text = '│',
+			numhl = 'GitSignsAddNr',
+			linehl = 'GitSignsAddLn',
+		},
+		change = {
+			hl = 'GitSignsChange',
+			text = '│',
+			numhl = 'GitSignsChangeNr',
+			linehl = 'GitSignsChangeLn',
+		},
+		delete = {
+			hl = 'GitSignsDelete',
+			text = '_',
+			numhl = 'GitSignsDeleteNr',
+			linehl = 'GitSignsDeleteLn',
+		},
+		topdelete = {
+			hl = 'GitSignsDelete',
+			text = '‾',
+			numhl = 'GitSignsDeleteNr',
+			linehl = 'GitSignsDeleteLn',
+		},
+		changedelete = {
+			hl = 'GitSignsChange',
+			text = '~',
+			numhl = 'GitSignsChangeNr',
+			linehl = 'GitSignsChangeLn',
+		},
+	},
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+
+	watch_gitdir = {interval = 1000, follow_files = true},
+	attach_to_untracked = true,
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+	},
+	current_line_blame_formatter_opts = {relative_time = false},
+	sign_priority = 6,
+	update_debounce = 100,
+	status_formatter = nil, -- Use default
+	max_file_length = 40000,
+	preview_config = {
+		-- Options passed to nvim_open_win
+		border = 'single',
+		style = 'minimal',
+		relative = 'cursor',
+		row = 0,
+		col = 1,
+	},
+	diff_opts = {internal = true}, -- If vim.diff or luajit is present
+	yadm = {enable = false},
 }
 
 return M
